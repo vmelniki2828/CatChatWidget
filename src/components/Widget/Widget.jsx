0ec836651f9ffe2f4f237e8048997cec60413a77
Kyrylo Dyachenko <104459315+kirill25460@users.noreply.github.com>
2024-09-17 21:03:15 +0300

    try

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -2,18 +2,15 @@ import React, { useState, useEffect } from 'react';
 
 import {
   WidgetCon,
-  
   JoinWrap,
   WidgetInputName,
   JoinButton,
   TextArea,
   SendBtn,
-  
   ChatText,
   MessageBox,
   ChatDiv,
   MessageWrap,
-  
   MessageTime,
   InfoWrap,
   CloseButton,
@@ -27,9 +24,8 @@ import {
   ClientInfoCont,
   SendBtnFile,
   FileInpIconWrapper,
-  
 } from './Widget.styled';
-import { socket } from '../../services/API'; // Убедитесь, что у вас правильно настроен путь
+import { socket } from '../../services/API'; 
 
 const Widget = ({ onClose }) => {
   const [username, setUsername] = useState('');
@@ -40,60 +36,70 @@ const Widget = ({ onClose }) => {
   const [roomId, setRoomId] = useState('');
   const [manager, setManager] = useState('');
 
-
+  
+  
   useEffect(() => {
     if (!socket) return;
 
     const savedUsername = sessionStorage.getItem('username');
     const savedUsermail = sessionStorage.getItem('usermail');
     const savedRoomId = sessionStorage.getItem('roomId');
-    const savedMessages = JSON.parse(sessionStorage.getItem('messages')) || [];
-    const savedManager = sessionStorage.getItem('manager'|| '');
-
-
+    const savedManager = sessionStorage.getItem('manager' || '');
 
     if (savedRoomId) {
-      console.log('Rejoining room:', savedRoomId );
+    
       setUsername(savedUsername);
       setUsermail(savedUsermail);
       setRoomId(savedRoomId);
-      setMessages(savedMessages);
-      setIsJoined(true);
       setManager(savedManager);
+      setIsJoined(true);
+
+      // Запрос сообщений из комнаты на сервере
       socket.emit('rejoin_user', { roomId: savedRoomId });
 
-      socket.on('roomRejoined', (messages) => {
+      socket.on('roomRejoined', (messagesFromServer) => {
         console.log('Successfully rejoined room:', savedRoomId);
-          setMessages(messages);
-          sessionStorage.setItem('messages', JSON.stringify(messages));
-        });
-      }
- 
-  
-
-    socket.on('manager_assigned', (managerData) => {
-      console.log('Назначен менеджер:', managerData);
-      setManager(managerData.username); 
-      sessionStorage.setItem('manager', managerData.username);
-    });
-
-    socket.on('receive_message', message => {
-      console.log('Received new message:', message);
-      setMessages(prevMessages => {
-        const updatedMessages = [...prevMessages, message];
-        sessionStorage.setItem('messages', JSON.stringify(updatedMessages));
-        return updatedMessages;
+        setMessages(messagesFromServer); // Устанавливаем сообщения с сервера
+        
       });
-    });
-
+    }
     return () => {
       socket.off('receive_message');
       socket.off('roomRejoined');
-      socket.off('manager_assigned');
+     
     };
   }, [socket]);
 
+  useEffect(() => {
   
+    socket.on('receive_message', message => {
+      console.log('Получено сообщение:', message);
+      setMessages(prevMessages => [...prevMessages, message]);
+    });
+
+    return () => {
+      socket.off('receive_message'); // Очистка обработчика при размонтировании
+    };
+  }, [socket]);
+  
+  useEffect(() => {
+    if (!socket) return;
+  
+    const handleManagerAssigned = (managerData) => {
+      console.log('Назначен менеджер:', managerData);
+      setManager(managerData.username);
+      sessionStorage.setItem('manager', managerData.username);
+    };
+  
+    socket.on('manager_assigned', handleManagerAssigned);
+  
+    return () => {
+      socket.off('manager_assigned', handleManagerAssigned);
+    };
+  }, [socket]);
+  
+  
+
   const getUserData = async () => {
     const userAgent = navigator.userAgent;
     const language = navigator.language;
@@ -103,8 +109,7 @@ const Widget = ({ onClose }) => {
 
     // Fetch IP and location using an external API (e.g., ipify, ipinfo)
     const response = await fetch('https://ipapi.co/json/'); // Example using ipapi
-    const { ip, city, region, country, latitude, longitude } =
-      await response.json();
+    const { ip, city, region, country, latitude, longitude } = await response.json();
 
     return {
       userAgent,
@@ -128,28 +133,25 @@ const Widget = ({ onClose }) => {
         otherInfo: userData,
       });
 
-      // Убедитесь, что идентификатор комнаты получен и сохранен правильно
-      socket.on('roomCreated', roomId => {
+      socket.on('roomCreated', (roomId) => {
         console.log('Room created:', roomId, socket);
-        setRoomId(roomId); // Установите идентификатор комнаты в состоянии
-        setIsJoined(true); // Устанавливаем состояние присоединения
+        setRoomId(roomId); 
+        setIsJoined(true); 
         sessionStorage.setItem('username', username.trim());
         sessionStorage.setItem('usermail', usermail.trim());
         sessionStorage.setItem('roomId', roomId);
-        sessionStorage.setItem('messages', JSON.stringify([]));
       });
     }
   };
-  
 
   const sendMessage = () => {
     if (message.trim() !== '') {
       socket.emit('send_message', {
-        roomId: roomId, // Используйте правильный идентификатор комнаты
+        roomId: roomId, 
         sender: username,
         messageText: message,
       });
-      setMessage(''); // Очищаем поле ввода
+      setMessage(''); 
     }
   };
 
@@ -162,7 +164,7 @@ const Widget = ({ onClose }) => {
       setMessages([]);
       setRoomId('');
       onClose();
-    }
+    }else{ onClose();}
   };
 
   const handleCollapse = () => {
@@ -170,7 +172,7 @@ const Widget = ({ onClose }) => {
   };
 
   return (
-    <WidgetCon >
+    <WidgetCon>
       <InfoWrap isJoined={isJoined}>
         <WidgetSettingsIcon />
         {manager}
@@ -184,12 +186,12 @@ const Widget = ({ onClose }) => {
         <JoinWrap>
           <WidgetInputName
             value={username}
-            onChange={e => setUsername(e.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
             placeholder="Введите ваше имя"
           />
           <WidgetInputName
             value={usermail}
-            onChange={e => setUsermail(e.target.value)}
+            onChange={(e) => setUsermail(e.target.value)}
             placeholder="Введите ваш email"
           />
           <JoinButton onClick={joinChat}>Присоединиться</JoinButton>
@@ -225,26 +227,19 @@ const Widget = ({ onClose }) => {
 
           <WidgetInput
             value={message}
-            onChange={e => setMessage(e.target.value)}
+            onChange={(e) => setMessage(e.target.value)}
             placeholder="Введите сообщение"
-            onKeyDown={e => {
+            onKeyDown={(e) => {
               if (e.key === 'Enter') {
-                sendMessage();  
-                e.preventDefault(); 
+                sendMessage();
+                e.preventDefault();
               }
             }}
           />
-          
-          <SendBtnFile
-            type="file"
-            id="fileUpload"
-            name="file"
-            multiple
-          />
-          <FileInpIconWrapper htmlFor="fileUpload"/>
-          <SendBtn onClick={sendMessage}/>
-           
-         
+
+          <SendBtnFile type="file" id="fileUpload" name="file" multiple />
+          <FileInpIconWrapper htmlFor="fileUpload" />
+          <SendBtn onClick={sendMessage} />
         </WrapArea>
       )}
     </WidgetCon>
